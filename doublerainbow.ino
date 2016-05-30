d8cdb32713514d0d1a7fb1b3e2f8d87b38034446
Hunter Fuller <hfuller@pixilic.com>
2016-05-30 18:30:28 -0500

    Add custom effects; improved loading of effects
    (Some effects stolen from github.com/hackhuntsville/globepixels)

--- doublerainbow.ino
+++ doublerainbow.ino
@@ -16,58 +16,119 @@ byte eep;
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(STRIPSIZE, PIN+1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(STRIPSIZE, PIN + 1, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(13,OUTPUT);
+  Serial.begin(38400);
+  pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
   strip.begin(); strip2.begin();
   strip.setBrightness(100); strip2.setBrightness(100); // Lower brightness and save eyeballs!
   strip.show(); strip2.show(); // Initialize all pixels to 'off'
   byte eep = EEPROM.read(0);
   if ( eep == 1 ) {
-    strip.setPixelColor(0,strip.Color(255,0,0));
-    state=1;
+    strip.setPixelColor(0, strip.Color(255, 0, 0));
+    state = 1;
   } else if ( eep == 2 ) {
-    strip.setPixelColor(0,strip.Color(0,255,0));
-    state=2;
+    strip.setPixelColor(0, strip.Color(0, 255, 0));
+    state = 2;
   } else {
-    strip.setPixelColor(0,strip.Color(0,0,255));
-    state=0;
+    strip.setPixelColor(0, strip.Color(0, 0, 255));
+    state = 0;
   }
   strip.show();
-  EEPROM.write(0,state+1);
+  EEPROM.write(0, state + 1);
   delay(2000);
   digitalWrite(13, LOW);
+  //state = 2; //for debug
 }
 
 void loop() {
   if ( state == 1 ) {
-    colorWave(75);
+    //colorWave(75);
+    runS_RAIN(75);
   } else if ( state == 2 ) {
-    rainbow(15);
+    //rainbow(15); //BROKEN
+    runS_PAPARAZZI(100);
   } else {
-    rainbowCycle(15);
+    //rainbowCycle(15); //BROKEN
+    colorWave(75);
+  }
+  strip.show(); strip2.show();
+}
+
+//Custom effects begin
+//Stolen from hackhuntsville on github (also partially my project tho)
+void runS_RAIN(byte lowValue) {
+
+  //fade out existing pixels
+  for ( int i = 0; i < STRIPSIZE; i++ ) {
+    byte x = (strip.getPixelColor(i));
+    if ( x > lowValue ) {
+      x -= 10;
+      strip.setPixelColor(i, strip.Color(x, x, x));
+    } else {
+      Serial.print(x); Serial.print("<"); Serial.println(lowValue);
+    }
+  }
+  for ( int i = 0; i < STRIPSIZE; i++ ) {
+    byte x = (strip2.getPixelColor(i));
+    if ( x > lowValue ) {
+      x -= 10;
+      strip2.setPixelColor(i, strip2.Color(x, x, x));
+    } else {
+      //Serial.print(x); Serial.print("<"); Serial.println(lowValue);
+    }
+  }
+  
+  //decide if we want to add a new raindrop
+  if ( random(0, 3) > 1 ) {
+    //we do
+    strip.setPixelColor(random(0, STRIPSIZE), strip.Color(255, 255, 255));
+  }
+  if ( random(0, 3) > 1 ) {
+    //we do
+    strip2.setPixelColor(random(0, STRIPSIZE), strip2.Color(255, 255, 255));
+  }
+
+  delay(40);
+}
+void runS_PAPARAZZI(byte blankVal) {
+  runS_BLANK(blankVal);
+  //decide if we want to add a new raindrop
+  if ( random(0, 100) < 99 ) {
+    //we do
+    int pos = random(0, STRIPSIZE);
+    for ( int i=0; i<3; i++ ) {
+      strip.setPixelColor(pos+i, strip.Color(255, 255, 255));
+      strip2.setPixelColor(pos+i, strip2.Color(255, 255, 255));
+    }
+  }
+  delay(random(75, 100));
+}
+void runS_BLANK(byte val) {
+  for ( int i = 0; i < STRIPSIZE; i++ ) {
+    strip.setPixelColor(i, val); strip2.setPixelColor(i, val);
   }
 }
+//Custom effects end
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
     }
-    strip.show();
     delay(wait);
   }
 }
@@ -76,11 +137,10 @@ void rainbow(uint8_t wait) {
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
-    strip.show();
     delay(wait);
   }
 }
@@ -88,28 +148,28 @@ void rainbowCycle(uint8_t wait) {
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  if (WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
 
 /**
- *      ^   ^   ^  
- * ~~~~~ ColorWave ~~~~~
- *        V   V   V   
- */
+        ^   ^   ^
+   ~~~~~ ColorWave ~~~~~
+          V   V   V
+*/
 void colorWave(uint8_t wait) {
   int i, j, stripsize, cycle;
   float ang, rsin, gsin, bsin, offset;
 
   static int tick = 0;
-  
+
   stripsize = strip.numPixels();
   cycle = stripsize * 25; // times around the circle...
 
@@ -119,8 +179,8 @@ void colorWave(uint8_t wait) {
     for (i = 0; i < stripsize; i++) {
       ang = map2PI(i) - offset;
       rsin = sin(ang);
-      gsin = sin(2.0 * ang / 3.0 + map2PI(int(stripsize/6)));
-      bsin = sin(4.0 * ang / 5.0 + map2PI(int(stripsize/3)));
+      gsin = sin(2.0 * ang / 3.0 + map2PI(int(stripsize / 6)));
+      bsin = sin(4.0 * ang / 5.0 + map2PI(int(stripsize / 3)));
       strip.setPixelColor(i, strip.Color(trigScale(rsin), trigScale(gsin), trigScale(bsin)));
       strip2.setPixelColor(i, strip.Color(trigScale(rsin), trigScale(gsin), trigScale(bsin)));
     }
@@ -132,12 +192,12 @@ void colorWave(uint8_t wait) {
 }
 
 /**
- * Scale a value returned from a trig function to a byte value.
- * [-1, +1] -> [0, 254] 
- * Note that we ignore the possible value of 255, for efficiency,
- * and because nobody will be able to differentiate between the
- * brightness levels of 254 and 255.
- */
+   Scale a value returned from a trig function to a byte value.
+   [-1, +1] -> [0, 254]
+   Note that we ignore the possible value of 255, for efficiency,
+   and because nobody will be able to differentiate between the
+   brightness levels of 254 and 255.
+*/
 byte trigScale(float val) {
   val += 1.0; // move range to [0.0, 2.0]
   val *= 127.0; // move range to [0.0, 254.0]
@@ -146,8 +206,8 @@ byte trigScale(float val) {
 }
 
 /**
- * Map an integer so that [0, striplength] -> [0, 2PI]
- */
+   Map an integer so that [0, striplength] -> [0, 2PI]
+*/
 float map2PI(int i) {
-  return PI*2.0*float(i) / float(strip.numPixels());
+  return PI * 2.0 * float(i) / float(strip.numPixels());
 }
